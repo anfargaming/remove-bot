b8100609692efe0745c5c73c4a989c71847bf99a
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-28 14:20:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config(object):
     API_ID = int(os.environ.get('API_ID'))
     API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    MONGO_URI = os.environ.get('MONGO_URI')
+    MONGODB_URI = os.environ.get('MONGODB_URI')
