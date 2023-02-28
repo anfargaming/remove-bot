9b25eaa5d9746fe5e4a3ea5f22119f6f3867b4a3
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-28 15:39:13 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,26 +1,30 @@
 import os
 from pymongo import MongoClient
 
+class Database:
+    def __init__(self, mongo_url, db_name):
+        self.client = MongoClient(mongo_url)
+        self.db = self.client.get_database(db_name)
+        self.users_collection = self.db.get_collection('users')
+
 MONGODB_URL = os.environ.get('MONGODB_URL')
 if not MONGODB_URL:
     raise ValueError('MONGODB_URL environment variable not set')
 
-client = MongoClient(MONGODB_URL)
-db = client.get_database('Cluster0')
-users_collection = db.get_collection('users')
-
+DB_NAME = "Cluster0"
+db = Database(MONGODB_URL, DB_NAME)
 
 def add_user(user_id, user_name):
     user = {'_id': user_id, 'name': user_name}
-    result = users_collection.insert_one(user)
+    result = db.users_collection.insert_one(user)
     return result.inserted_id
 
 
 def get_user(user_id):
-    user = users_collection.find_one({'_id': user_id})
+    user = db.users_collection.find_one({'_id': user_id})
     return user
 
 
 def remove_user(user_id):
-    result = users_collection.delete_one({'_id': user_id})
+    result = db.users_collection.delete_one({'_id': user_id})
     return result.deleted_count
