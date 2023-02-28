209a2d2fa66c2a8284cfc6b707f4d650bfe746ca
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-28 15:47:51 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,24 +7,17 @@ class Database:
         self.db = self.client.get_database(db_name)
         self.users_collection = self.db.get_collection('users')
 
-MONGODB_URL = os.environ.get('MONGODB_URL')
-if not MONGODB_URL:
-    raise ValueError('MONGODB_URL environment variable not set')
-
-DB_NAME = "Cluster0"
-db = Database(MONGODB_URL, DB_NAME)
-
 def add_user(user_id, user_name):
     user = {'_id': user_id, 'name': user_name}
-    result = db.users_collection.insert_one(user)
+    result = Database.users_collection.insert_one(user)
     return result.inserted_id
 
 
 def get_user(user_id):
-    user = db.users_collection.find_one({'_id': user_id})
+    user = Database.users_collection.find_one({'_id': user_id})
     return user
 
 
 def remove_user(user_id):
-    result = db.users_collection.delete_one({'_id': user_id})
+    result = Database.users_collection.delete_one({'_id': user_id})
     return result.deleted_count
